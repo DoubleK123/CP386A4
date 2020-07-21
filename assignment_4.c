82ab876613a201d3058f4f8bc0308fee066aea27
hamdan kasem <58044223+kase4140@users.noreply.github.com>
2020-07-21 17:51:22 -0400

    skeleton code for our github assignment 4
    creating a skeleton code for our bankers algorithm for avoiding a deadlock.

---
+++ assignment_4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "assignment_4.h"
+
+int main(int argc, char* argv[]){
+
+}
+
+void read_file(char* fileName){
+
+}
+
+void RQ(char* command){
+
+}
+
+void RL(char* command){
+
+}
+
+void run(){
+
+}
+
+int safetyAlgorithm(){
+
+}
---
+++ assignment_4.h
@@ -0,0 +1,6 @@
+int read_file(char* fileName);
+void RQ(char* command);
+void RL(char*  command);
+void asterisk();
+void run();
+int safetyAlgorithm();
