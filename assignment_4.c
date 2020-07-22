f066578c560c0aae25c25dd9060cfc88f0a2a198
hamdan kasem <58044223+kase4140@users.noreply.github.com>
2020-07-22 17:37:00 -0400

    reading the arguments
    reading the arguments and putting them in a list of ints and also calling the function read_file and passing it the sample file in

--- assignment_4.c
+++ assignment_4.c
@@ -3,11 +3,29 @@
 #include "assignment_4.h"
 
 int main(int argc, char* argv[]){
+    
+    int available[argc];
+    int i;
+    if(argc>0){
+        for(i=0;i<argc;i++){
+            // printf("argv: %d\n",atoi(argv[i]));
+            available[i]=atoi(argv[i]);
+            // printf("available: %d\n", available[i]);
+        }
+    }else{
+        printf("no arguments have been given check again\n");
+    }
 
+    char* fileName = "sample4_in.txt";
+    int result = read_file(fileName);
+    printf("read: %d\n",result);
+
+    return 0;
 }
 
-void read_file(char* fileName){
+int read_file(char* fileName){
 
+    return 0;
 }
 
 void RQ(char* command){
@@ -24,4 +42,5 @@ void run(){
 
 int safetyAlgorithm(){
 
+    return 0;
 }
