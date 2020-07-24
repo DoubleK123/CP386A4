86782d5e94042cc1be21418b4b43387e6453b3a5
leviathan <hamdankasem4@gmail.com>
2020-07-24 15:41:53 -0400

    added a make file and fixed minor issues

--- assignment_4.c
+++ assignment_4.c
@@ -9,17 +9,23 @@
 
 int main(int argc, char* argv[]){
     
-    int available[argc];
+    int available[4];
     int maximum[NUM_OF_RES][NUM_OF_CUST];
     int i;
     if(argc>0){
-        for(i=0;i<argc;i++){
+        int index=0;
+        for(i=1;i<argc;i++){
             // printf("argv: %d\n",atoi(argv[i]));
-            available[i]=atoi(argv[i]);
+            available[index]=atoi(argv[i]);
+            index++;
             // printf("available: %d\n", available[i]);
         }
     }else{
         printf("no arguments have been given check again\n");
+    }
+
+    for(int i=0;i <4;i++){
+        printf("available: %d\n", available[i]);
     }
 
     // read file
@@ -59,6 +65,7 @@ int read_file(char* fileName, int* maximum){
                 index++;
             }
         }
+        
         for(int i=0; i< NUM_OF_RES;i++){
             for(int j=0;j<NUM_OF_CUST;j++){
                 *(maximum+i*NUM_OF_CUST+j)=array[i*NUM_OF_CUST+j];
